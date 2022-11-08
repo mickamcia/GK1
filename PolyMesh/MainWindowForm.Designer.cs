90ea468864912b9b7effb7122896703c17558b6a
mickamcia <mkciasnocha@gmail.com>
2022-11-08 01:51:01 +0100

    obj parsed

--- PolyMesh/MainWindowForm.Designer.cs
+++ PolyMesh/MainWindowForm.Designer.cs
@@ -28,19 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.MainPictureBox = new System.Windows.Forms.PictureBox();
+            this.MainGroupBox = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // MainPictureBox
+            // 
+            this.MainPictureBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MainPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.MainPictureBox.Name = "MainPictureBox";
+            this.MainPictureBox.Size = new System.Drawing.Size(800, 800);
+            this.MainPictureBox.TabIndex = 0;
+            this.MainPictureBox.TabStop = false;
+            this.MainPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.MainPictureBox_Paint);
+            // 
+            // MainGroupBox
+            // 
+            this.MainGroupBox.Dock = System.Windows.Forms.DockStyle.Right;
+            this.MainGroupBox.Location = new System.Drawing.Point(806, 0);
+            this.MainGroupBox.Name = "MainGroupBox";
+            this.MainGroupBox.Size = new System.Drawing.Size(200, 800);
+            this.MainGroupBox.TabIndex = 1;
+            this.MainGroupBox.TabStop = false;
+            this.MainGroupBox.Text = "Controls";
             // 
             // MainWindowForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1006, 800);
+            this.Controls.Add(this.MainGroupBox);
+            this.Controls.Add(this.MainPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "MainWindowForm";
             this.Text = "PolyMesh";
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private PictureBox MainPictureBox;
+        private GroupBox MainGroupBox;
     }
 }
--- PolyMesh/MainWindowForm.cs
+++ PolyMesh/MainWindowForm.cs
@@ -1,10 +1,45 @@
+using System.Drawing;
+
 namespace PolyMesh
 {
     public partial class MainWindowForm : Form
     {
+        const string path = "C:\\Users\\s\\Source\\Repos\\mickamcia\\GK1\\PolyMesh\\sphere.obj";
+        public List<Vertex> vertices;
+        public List<Edge> edges;
+        public Bitmap bits;
         public MainWindowForm()
         {
             InitializeComponent();
+            vertices = new List<Vertex>();
+            edges = new List<Edge>();
+            bits = new(800, 800);
+            for(int i = 0; i < Data.Raw.Length; i+=3)
+            {
+                vertices.Add(new Vertex(Data.Raw[i], Data.Raw[i + 1], Data.Raw[i + 2]));
+            }
+            for (int i = 0; i < Data.Pairs.Length; i += 6)
+            {
+                edges.Add(new Edge(vertices[Data.Pairs[i] - 1], vertices[Data.Pairs[i + 2] - 1]));
+                edges.Add(new Edge(vertices[Data.Pairs[i + 2] - 1], vertices[Data.Pairs[i + 4] - 1]));
+                edges.Add(new Edge(vertices[Data.Pairs[i + 4] - 1], vertices[Data.Pairs[i] - 1]));
+            }
+            MainPictureBox.Invalidate();
+        }
+
+        private void MainPictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            using var g = Graphics.FromImage(bits);
+            g.Clear(Color.White);
+            foreach (var v in vertices)
+            {
+                bits.SetPixel((int)(v.a * 400) + 400, (int)(v.b * 400) + 400, Color.Black);
+            }
+            foreach (var uv in edges)
+            {
+                g.DrawLine(Pens.Black, (int)(uv.v1.a * 400) + 400, (int)(uv.v1.b * 400) + 400, (int)(uv.v2.a * 400) + 400, (int)(uv.v2.b * 400) + 400);
+            }
+            e.Graphics.DrawImage(bits, 0, 0);
         }
     }
 }
